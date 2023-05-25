5ab1c96257b0d5c745a466c1ae1cb0e9fa4ba2f0
AkiraRei <Akirarei@yandex.ru>
2023-05-25 17:08:28 +0300

    IBOutlets making

--- ColorApp/ViewController.swift
+++ ColorApp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var textFieldRed: UITextField!
+    @IBOutlet var textFieldGreen: UITextField!
+    @IBOutlet var textFieldBlue: UITextField!
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: IBActions
+    @IBAction func sliderRedAction() {
+        
+    }
+    @IBAction func sliderGreenAction() {
+        
+    }
+    @IBAction func sliderBlueAction() {
+        
+    }
+    
 
 }
 
